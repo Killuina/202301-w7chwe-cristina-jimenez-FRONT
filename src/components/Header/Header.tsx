9164dea3f7e04731d861b69729a26e8bb65d40f9
Cristina <cristinajmartin98@gmail.com>
2023-02-26 22:43:41 +0100

    Add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): JSX.Element => {
+  return (
+    <HeaderStyled>
+      <h2>Feisbuk</h2>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
