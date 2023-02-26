cef83dd28433bed366df4d31a1e5e1283cdc7349
Cristina <cristinajmartin98@gmail.com>
2023-02-26 16:44:41 +0100

    Add Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -7,8 +7,22 @@ const Login = (): JSX.Element => {
 
   const emptyAreaFields = username === "" || password === "";
 
+  const handleUsername = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(value);
+  };
+
+  const handlePassword = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(value);
+  };
+
   const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event?.preventDefault();
+
+    document.querySelectorAll("input").forEach((input) => (input.value = ""));
 
     setUsername("");
     setPassword("");
@@ -19,12 +33,22 @@ const Login = (): JSX.Element => {
       <label htmlFor="username" className="form__label">
         Username:
       </label>
-      <input type="text" className="form__input" id="username" />
+      <input
+        type="text"
+        className="form__input"
+        id="username"
+        onChange={handleUsername}
+      />
       <label htmlFor="password" className="form__label">
         Password:
       </label>
-      <input type="password" className="form__input" id="password" />
-      <button className="form__button" disabled={emptyAreaFields}>
+      <input
+        type="password"
+        className="form__input"
+        id="password"
+        onChange={handlePassword}
+      />
+      <button type="submit" className="form__button" disabled={emptyAreaFields}>
         Log in
       </button>
     </LoginStyled>
